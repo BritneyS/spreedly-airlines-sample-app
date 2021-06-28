8f1e6dc9c41109427cca08f53575f28e99d9ac83
Britney Smith <britneysmith@Britneys-MBP.twmi.rr.com>
2021-06-28 14:54:53 -0400

    Installed and ran prettier

--- client/src/data/flights.js
+++ client/src/data/flights.js
@@ -1,36 +1,36 @@
 const flights = [
-  {
-    id: 1,
-    from: "Detroit, MI",
-    fromIATA: "DTW",
-    to: "Chicago, IL",
-    toIATA: "ORD",
-    priceInCents: "12500",
-  },
-  {
-    id: 2,
-    from: "Los Angeles, CA",
-    fromIATA: "LAX",
-    to: "Charlotte, NC",
-    toIATA: "CLT",
-    priceInCents: "55000",
-  },
-  {
-    id: 3,
-    from: "Denver, CO",
-    fromIATA: "DEN",
-    to: "Detroit, MI",
-    toIATA: "DTW",
-    priceInCents: "42500",
-  },
-  {
-    id: 4,
-    from: "Dallas, TX",
-    fromIATA: "DFW",
-    to: "Baltimore, MD",
-    toIATA: "BWI",
-    priceInCents: "34500",
-  },
-];
+    {
+        "id": 1,
+        "from": "Detroit, MI",
+        "fromIATA": "DTW",
+        "to": "Chicago, IL",
+        "toIATA": "ORD",
+        "priceInCents": "12500"
+    },
+    {
+        "id": 2,
+        "from": "Los Angeles, CA",
+        "fromIATA": "LAX",
+        "to": "Charlotte, NC",
+        "toIATA": "CLT",
+        "priceInCents": "55000"
+    },
+    {
+        "id": 3,
+        "from": "Denver, CO",
+        "fromIATA": "DEN",
+        "to": "Detroit, MI",
+        "toIATA": "DTW",
+        "priceInCents": "42500"
+    },
+    {
+        "id": 4,
+        "from": "Dallas, TX",
+        "fromIATA": "DFW",
+        "to": "Baltimore, MD",
+        "toIATA": "BWI",
+        "priceInCents": "34500"
+    },
+]
 
-export default flights;
+export default flights
