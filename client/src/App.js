a61f471f33dc55b52fecd6b0a4d3e7ac99cd9da7
Britney Smith <britneysmith@Britneys-MBP.twmi.rr.com>
2021-06-28 16:05:45 -0400

    Build frontend display of fake flights

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import flights from "./data/flights";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main className="App">
+      <header className="block mt-6">
+        <h1 className="title is-1 has-text-grey-dark">Book A Flight</h1>
       </header>
-    </div>
+      <section className="columns is-justify-content-center">
+        <ul>
+          {!flights
+            ? "Loading..."
+            : flights.map((flight) => {
+                return (
+                  <li key={flight.id} className="box shop-card column">
+                    <div className="media-content">
+                      <h2 className="is-size-5 has-text-weight-semibold">
+                        {flight.from} / {flight.fromIATA} to {flight.to} /{" "}
+                        {flight.toIATA}
+                      </h2>
+                      <p className="is-size-6">
+                        Price:{" "}
+                        <span className="has-text-weight-semibold has-text-primary-dark">
+                          ${flight.priceInCents / 100}
+                        </span>
+                      </p>
+                      <button className="button is-primary has-text-weight-semibold">
+                        Book ✈️
+                      </button>
+                    </div>
+                  </li>
+                );
+              })}
+        </ul>
+      </section>
+    </main>
   );
 }
 
